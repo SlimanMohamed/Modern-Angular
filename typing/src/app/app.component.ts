a3ab2d1b3efcee9b57f05818285e5bb69dc44f2b
SLIMAN Mohamed <mohamedsliman789@gmail.com>
2022-04-13 15:04:58 +0000

    Typing Game ultimate version

--- typing/src/app/app.component.ts
+++ typing/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { compileDeclareDirectiveFromMetadata } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import{lorem} from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+randomText = lorem.sentence();
+enteredText='';
+  OnInit(value:string){
+this.enteredText=value;
+}
+
+compare(randomLetter:string, enteredLetter:string){
+  if(!enteredLetter){
+    return 'pending';
+  }
+ return randomLetter === enteredLetter ? 'correct':'incorrect';
+}
 }
